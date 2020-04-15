fbb3f1d1b69cd91b607c255f8208d821b164449f
Jonas Bay Andreasen <au616208@uni.au.dk>
2020-04-15 09:27:51 +0200

    Jonas

---
+++ Morgenmadsbuffet/Models/CheckInsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Morgenmadsbuffet.Models
+{
+    public class CheckInsModel
+    {
+        // Keys
+        public int RoomId { get; set; }
+        public string Date { get; set; }
+        public int AdultCount { get; set; }
+        public int ChildCount { get; set; }
+    }
+}
